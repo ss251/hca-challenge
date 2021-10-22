0a64d4643bec192108a663f70a4d5bef40f9de29
ss251 <ss251@uw.edu>
2021-10-22 00:41:02 -0500

    styling changes

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -2,28 +2,28 @@ import React from "react";
 
 const VideoCard = ({ video }) => {
   return (
-    <div className="video-holder">
-      <a
-        className="list-group-item list-group-item-action flex-column align-items-start"
-        href="/"
-      >
+    <div className="card">
+      <div className="card-horizontal">
         <div className="video-thumb">
           <img
-            className="thumb-img"
+            className="img-square-wrapper"
             src={video.snippet.thumbnails.medium.url}
             alt={video.snippet.description}
           />
         </div>
-        <div className="video-title">
-          <div className="d-flex w-100 justify-content-between">
+
+        <div className="video-details">
+          <div className="card-title">
             <h5 className="mb-1">{video.snippet.title}</h5>
           </div>
+          <div className="card-text">
+            <div className="video-descr">
+              <p className="mb-1">{video.snippet.description}</p>
+            </div>
+            <div className="video-date">{video.snippet.publishedAt}</div>
+          </div>
         </div>
-        <div className="video-descr">
-          <p className="mb-1">{video.snippet.description}</p>
-        </div>
-        <div className="video-date">{video.snippet.publishedAt}</div>
-      </a>
+      </div>
     </div>
   );
 };
