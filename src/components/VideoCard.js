48ba300500a65642574c83b4c6df928cd8bf50a4
ss251 <ss251@uw.edu>
2021-10-22 02:11:49 -0500

    video pop-up added

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import ModalVideo from "react-modal-video";
+// getting the help of below package to render modal pop-up
+import "react-modal-video/scss/modal-video.scss";
 
 const VideoCard = ({ video }) => {
+  const [isOpen, setOpen] = useState(false);
+  var videoId = video.id.videoId;
+  /* video ids for videos rendered from a playlist are accessed 
+     differently than videos not rendered from playlist
+     COVID-19 videos are rendered as playlist items accessed
+     from the 'COVID-19 Vaccine Podcast' playlist id.*/
+  if (typeof videoId === "undefined") {
+    videoId = video.snippet.resourceId.videoId;
+  }
   return (
     <div className="card">
+      <ModalVideo
+        channel="youtube"
+        autoplay
+        isOpen={isOpen}
+        videoId={videoId}
+        onClose={() => setOpen(false)}
+      />
       <div className="card-horizontal">
         <div className="video-thumb">
           <img
             className="img-square-wrapper"
             src={video.snippet.thumbnails.medium.url}
             alt={video.snippet.description}
+            onClick={() => setOpen(true)}
           />
         </div>
 
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -3,7 +3,13 @@ import VideoCard from "./VideoCard";
 
 const VideoList = ({ videos }) => {
   const videoList = videos.map((video) => {
-    return <VideoCard key={video.id.videoId} video={video} />;
+    return (
+      <VideoCard
+        // unique keys were missing in covid-19 video cards
+        key={video.id.videoId || video.snippet.resourceId.videoId}
+        video={video}
+      />
+    );
   });
   return <div className="list-group">{videoList}</div>;
 };
