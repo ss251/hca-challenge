a96f6177257e2df068c15e8ad9bb2fbf7bc9f24c
ss251 <ss251@uw.edu>
2021-10-22 18:22:16 -0500

    remove app.js

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,16 +2,14 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import All from "../pages/All";
 import Covid from "../pages/Covid";
-import App from "./App";
 import NotExist from "./NotExist";
 
 // routing to components
 const Router = () => (
   <BrowserRouter>
     <Switch>
-      <Route exact path="/" component={App} />
+      <Route exact path="/" component={Covid} />
       <Route exact path="/all" component={All} />
-      <Route exact path="/covid" component={Covid} />
       <Route component={NotExist} />
     </Switch>
   </BrowserRouter>
